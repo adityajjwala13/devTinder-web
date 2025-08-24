60ca617310221fba8824027f3159e085d6df7731
Aditya <adityajhunjhunwala13@gmail.com>
2025-08-24 23:21:18 +0530

    Added Authentication in Routing

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   const user = useSelector((store) => store.user);
   return (
     <div className="navbar bg-base-300 shadow-sm">
       <div className="flex-1">
-        <a className="btn btn-ghost text-xl">🧑🏻‍💻DevTinder</a>
+        <Link to="/" className="btn btn-ghost text-xl">
+          🧑🏻‍💻DevTinder
+        </Link>
       </div>
       <div className="flex gap-2">
         <div className="dropdown dropdown-end">
@@ -32,16 +35,16 @@ const Navbar = () => {
             className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
           >
             <li>
-              <a className="justify-between">
+              <Link to="/profile" className="justify-between">
                 Profile
                 <span className="badge">New</span>
-              </a>
+              </Link>
             </li>
             <li>
-              <a>Settings</a>
+              <Link to="/">Settings</Link>
             </li>
             <li>
-              <a>Logout</a>
+              <Link to="/profile">Logout</Link>
             </li>
           </ul>
         </div>
